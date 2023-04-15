0efbabb4c19f6775f62eef042ca1d40c2e23840d
Sil-Estrada <silestrada@gmail.com>
2023-04-15 19:49:53 -0300

    base3

--- coder51325/app51325/models.py
+++ coder51325/app51325/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 
 class cliente(models.Model):
-    nombre = models.CharField(max_length=20)
+    cli_nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     dni = models.IntegerField()
-    nombre = models.EmailField()
+    mail = models.EmailField()
 
 class proveedor(models.Model):
-    nombre = models.CharField(max_length=30)
+    provee_nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=30)
     cuit = models.CharField(max_length=11)
     telefono = models.IntegerField()
 
 class producto(models.Model):
+    cod_prod= models.IntegerField()
     nombre = models.CharField(max_length=15)
     rubro = models.CharField(max_length=10)
     precio = models.FloatField()
     nom_proveedor = models.CharField(max_length=30)
+
+class carrito(models.Model):
+    cli_nombre = models.CharField(max_length=20)
+    cod_prod= models.IntegerField()
+    nombre = models.CharField(max_length=15)
+    cantidad_unidades = models.IntegerField()
+
+
+    
+
+
     
 
 
